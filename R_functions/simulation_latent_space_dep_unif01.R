2205b9b63424faad577e80fc2a3af7e33d96b718
donga0223 <44006266+donga0223@users.noreply.github.com>
2020-11-18 23:13:49 -0500

    Add files via upload

--- R_functions/simulation_latent_space_dep_unif01.R
+++ R_functions/simulation_latent_space_dep_unif01.R
@@ -15,67 +15,67 @@ sim_res7 <- sim_res8 <- sim_res9 <- sim_res10 <- sim_res11 <- sim_res12 <- list(
 
 
 sim_res1 <- foreach(i = 1:iter, .combine='rbind', .packages=c('network', 'MASS', 'RDS')) %dopar% {
-  permutation_test(gen_latent_space(Sigma = NULL, maxunif = 1, sample.size = 2000, myseed = i, alpha = 0-0.5, beta = c(0.1,0.5), a = 0.1, b = 1)
+  latentspace_SPRTBA(gen_latent_space(Sigma = NULL, maxunif = 1, sample.size = 2000, myseed = i, alpha = 0-0.5, beta = c(0.1,0.5), a = 0.1, b = 1)
                    , RDS.size = 500, number.of.seeds = 10, per.iter = 1000, correct = T)
 }
 
 sim_res2 <- foreach(i = 1:iter, .combine='rbind', .packages=c('network', 'MASS', 'RDS')) %dopar% {
-  permutation_test(gen_latent_space(Sigma = NULL, maxunif = 1, sample.size = 2000, myseed = i, alpha = 0-3, beta = c(0.1,3), a = 0.1, b = 1)
+  latentspace_SPRTBA(gen_latent_space(Sigma = NULL, maxunif = 1, sample.size = 2000, myseed = i, alpha = 0-3, beta = c(0.1,3), a = 0.1, b = 1)
                    , RDS.size = 500, number.of.seeds = 10, per.iter = 1000, correct = T)
 }
 
 sim_res3 <- foreach(i = 1:iter, .combine='rbind', .packages=c('network', 'MASS', 'RDS')) %dopar% {
-  permutation_test(gen_latent_space(Sigma = NULL, maxunif = 1, sample.size = 2000, myseed = i, alpha = 0-3, beta = c(1,3), a = 0.1, b = 1)
+  latentspace_SPRTBA(gen_latent_space(Sigma = NULL, maxunif = 1, sample.size = 2000, myseed = i, alpha = 0-3, beta = c(1,3), a = 0.1, b = 1)
                    , RDS.size = 500, number.of.seeds = 10, per.iter = 1000, correct = T)
 }
 
 
 sim_res4 <- foreach(i = 1:iter, .combine='rbind', .packages=c('network', 'MASS', 'RDS')) %dopar% {
-  permutation_test(gen_latent_space(Sigma = NULL, maxunif = 1, sample.size = 2000, myseed = i, alpha = 0-0.5, beta = c(0.1,0.5), a = 0.3, b = 1)
+  latentspace_SPRTBA(gen_latent_space(Sigma = NULL, maxunif = 1, sample.size = 2000, myseed = i, alpha = 0-0.5, beta = c(0.1,0.5), a = 0.3, b = 1)
                    , RDS.size = 500, number.of.seeds = 10, per.iter = 1000, correct = T)
 }
 
 sim_res5 <- foreach(i = 1:iter, .combine='rbind', .packages=c('network', 'MASS', 'RDS')) %dopar% {
-  permutation_test(gen_latent_space(Sigma = NULL, maxunif = 1, sample.size = 2000, myseed = i, alpha = 0-3, beta = c(0.1,3), a = 0.3, b = 1)
+  latentspace_SPRTBA(gen_latent_space(Sigma = NULL, maxunif = 1, sample.size = 2000, myseed = i, alpha = 0-3, beta = c(0.1,3), a = 0.3, b = 1)
                    , RDS.size = 500, number.of.seeds = 10, per.iter = 1000, correct = T)
 }
 
 sim_res6 <- foreach(i = 1:iter, .combine='rbind', .packages=c('network', 'MASS', 'RDS')) %dopar% {
-  permutation_test(gen_latent_space(Sigma = NULL, maxunif = 1, sample.size = 2000, myseed = i, alpha = 0-3, beta = c(1,3), a = 0.3, b = 1)
+  latentspace_SPRTBA(gen_latent_space(Sigma = NULL, maxunif = 1, sample.size = 2000, myseed = i, alpha = 0-3, beta = c(1,3), a = 0.3, b = 1)
                    , RDS.size = 500, number.of.seeds = 10, per.iter = 1000, correct = T)
 }
 
 sim_res7 <- foreach(i = 1:iter, .combine='rbind', .packages=c('network', 'MASS', 'RDS')) %dopar% {
-  permutation_test(gen_latent_space(Sigma = NULL, maxunif = 1, sample.size = 2000, myseed = i, alpha = 0-0.5, beta = c(0.1,0.5), a = 0.5, b = 1)
+  latentspace_SPRTBA(gen_latent_space(Sigma = NULL, maxunif = 1, sample.size = 2000, myseed = i, alpha = 0-0.5, beta = c(0.1,0.5), a = 0.5, b = 1)
                    , RDS.size = 500, number.of.seeds = 10, per.iter = 1000, correct = T)
 }
 
 sim_res8 <- foreach(i = 1:iter, .combine='rbind', .packages=c('network', 'MASS', 'RDS')) %dopar% {
-  permutation_test(gen_latent_space(Sigma = NULL, maxunif = 1, sample.size = 2000, myseed = i, alpha = 0-3, beta = c(0.1,3), a = 0.5, b = 1)
+  latentspace_SPRTBA(gen_latent_space(Sigma = NULL, maxunif = 1, sample.size = 2000, myseed = i, alpha = 0-3, beta = c(0.1,3), a = 0.5, b = 1)
                    , RDS.size = 500, number.of.seeds = 10, per.iter = 1000, correct = T)
 }
 
 sim_res9 <- foreach(i = 1:iter, .combine='rbind', .packages=c('network', 'MASS', 'RDS')) %dopar% {
-  permutation_test(gen_latent_space(Sigma = NULL, maxunif = 1, sample.size = 2000, myseed = i, alpha = 0-3, beta = c(1,3), a = 0.5, b = 1)
+  latentspace_SPRTBA(gen_latent_space(Sigma = NULL, maxunif = 1, sample.size = 2000, myseed = i, alpha = 0-3, beta = c(1,3), a = 0.5, b = 1)
                    , RDS.size = 500, number.of.seeds = 10, per.iter = 1000, correct = T)
 }
 
 sim_res10 <- foreach(i = 1:iter, .combine='rbind', .packages=c('network', 'MASS', 'RDS')) %dopar% {
-  permutation_test(gen_latent_space(Sigma = NULL, maxunif = 1, sample.size = 2000, myseed = i, alpha = 0-0.5, beta = c(0.1,0.5), a = 0.2, b = 1)
+  latentspace_SPRTBA(gen_latent_space(Sigma = NULL, maxunif = 1, sample.size = 2000, myseed = i, alpha = 0-0.5, beta = c(0.1,0.5), a = 0.2, b = 1)
                    , RDS.size = 500, number.of.seeds = 10, per.iter = 1000, correct = T)
 }
 
 sim_res11 <- foreach(i = 1:iter, .combine='rbind', .packages=c('network', 'MASS', 'RDS')) %dopar% {
-  permutation_test(gen_latent_space(Sigma = NULL, maxunif = 1, sample.size = 2000, myseed = i, alpha = 0-3, beta = c(0.1,3), a = 0.2, b = 1)
+  latentspace_SPRTBA(gen_latent_space(Sigma = NULL, maxunif = 1, sample.size = 2000, myseed = i, alpha = 0-3, beta = c(0.1,3), a = 0.2, b = 1)
                    , RDS.size = 500, number.of.seeds = 10, per.iter = 1000, correct = T)
 }
 
 sim_res12 <- foreach(i = 1:iter, .combine='rbind', .packages=c('network', 'MASS', 'RDS')) %dopar% {
-  permutation_test(gen_latent_space(Sigma = NULL, maxunif = 1, sample.size = 2000, myseed = i, alpha = 0-3, beta = c(1,3), a = 0.2, b = 1)
+  latentspace_SPRTBA(gen_latent_space(Sigma = NULL, maxunif = 1, sample.size = 2000, myseed = i, alpha = 0-3, beta = c(1,3), a = 0.2, b = 1)
                    , RDS.size = 500, number.of.seeds = 10, per.iter = 1000, correct = T)
 }
 
-save.image(file="permutation_latent_space_dep_unif01.RData")
+save.image(file="latent_space_SPRTBA_dep_unif01.RData")
 
 plot(sort(sim_res1$chisq.class.group), type = "l")
 lines(sort(sim_res1$RDS.chisq), type = "l", col = 2)
